175d9c66ec03ccdbbbc1865fd4ac1520e7563afd
Vishesht Gupta <vgupta@ccrn.com>
2024-09-17 18:12:38 +0530

    BaseUrl change

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,5 +1,5 @@
-export const BaseUrl = "http://localhost:5001";
-// export const BaseUrl = "https://hi-influencer-nodejs.onrender.com";
+// export const BaseUrl = "http://localhost:5001";
+export const BaseUrl = "https://hi-influencer-nodejs.onrender.com";
 export const ProfileCheckRegex = /^\/user\/[^\/]+$/;
 
 export const menPlaceholderImg =
