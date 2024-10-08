81239492b9926a9e947eff637a6abcd34c673029
Vishesht Gupta <vgupta@ccrn.com>
2024-10-08 17:01:48 +0530

    redirects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
+import { ProfileCheckRegex } from "@/common/utils";
 import Home from "@/components/Home";
 import { useAppSelector } from "@/lib/hooks";
 import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function MainLayout() {
   const path = usePathname();
@@ -10,12 +12,36 @@ export default function MainLayout() {
   const isAdmin = loginData?.isAdmin;
   const data = loginData?.userData;
 
-  // if (!data) {
-  //   if (path === "/" || path === "/login" || path === "/signup") {
+  const useProfilePathCheck = () => {
+    const profilePathRegex = ProfileCheckRegex;
+    return profilePathRegex.test(path);
+  };
 
-  //   } else {
-  //     router.push("/signup");
-  //   }
-  // }
+  const isUserProfile: boolean = useProfilePathCheck();
+
+  useEffect(() => {
+    // Handle admin access logic
+    if (isAdmin) {
+      // If an admin is trying to access /admin, redirect to /admin/dashboard
+      if (path === "/admin") {
+        router.push("/admin/dashboard");
+      }
+    } else {
+      if (path === "/admin" || path.startsWith("/admin")) {
+        router.push("/admin");
+      }
+    }
+    if (!data) {
+      if (
+        path !== "/login" &&
+        path !== "/signup" &&
+        !isUserProfile &&
+        !path.startsWith("/admin")
+      ) {
+        router.push("/login");
+      }
+    }
+  }, [path, router, isAdmin, isUserProfile]);
+
   return <Home />;
 }
