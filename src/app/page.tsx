e0d9d13d85377773800ce29c104cc4cde82387b0
Vishesht Gupta <vgupta@ccrn.com>
2024-10-08 16:27:36 +0530

    check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 "use client";
+import { useAppSelector } from "@/lib/hooks";
 import { usePathname, useRouter } from "next/navigation";
 
 export default function MainLayout() {
   const path = usePathname();
   const router = useRouter();
+  const loginData = useAppSelector((state) => state.login);
+  const isAdmin = loginData?.isAdmin;
+  const data = loginData?.userData;
 
-  if (path === "/") {
-    router.push("/home");
+  if (data) {
+    if (path === "/" || path === "/login" || path === "/signup") {
+      router.push("/home");
+    } else {
+      router.push(path);
+    }
   }
 }
